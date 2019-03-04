7fb3ed7f4f23193b93c068a79d999cf172b07e8d
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-04 05:06:02 +0000

    Automatically backed up by Learn

--- actions/index.js
+++ actions/index.js
@@ -10,16 +10,18 @@ import fileStore from '../stores/fileStore';
 // order to "update" state in some form or another.
 
 const addFile = () => {
+  fileStore.addFile();
 };
 
 const removeFile = (index) => {
+  fileStore.removeFile(index);
 };
 
 const updateFile = (index, file) => {
+  fileStore.updateFile(index, file);
 };
 
 export default {
   addFile,
   removeFile,
   updateFile,
-};
